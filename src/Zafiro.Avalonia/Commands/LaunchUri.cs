bdd8e719eca74d00acc4107943f5ac0ae5d778de
José Manuel Nieto <superjmn@outlook.com>
2025-11-15 15:15:10 +0100

    Refactor global commands into configurable container

--- src/Zafiro.Avalonia/Commands/LaunchUri.cs
+++
@@ -1,7 +0,0 @@
-using CSharpFunctionalExtensions;
-using Zafiro.Avalonia.Services;
-using Zafiro.UI.Commands;
-
-namespace Zafiro.Avalonia.Commands;
-
-public class LaunchUri(ILauncherService launcherService) : EnhancedCommandWrapper<Uri, Result>(ReactiveCommand.CreateFromTask<Uri, Result>(str => Result.Try(() => launcherService.LaunchUri(str))).Enhance());
