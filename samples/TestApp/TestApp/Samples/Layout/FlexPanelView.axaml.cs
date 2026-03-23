5f40fb25860940839303df4c41e90361c5d122ee
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2026-03-23 23:55:10 +0100

    feat: rename FlexBox to FlexPanel and add responsive sample

---
+++ samples/TestApp/TestApp/Samples/Layout/FlexPanelView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TestApp.Samples.Layout;
+
+public partial class FlexPanelView : UserControl
+{
+    public FlexPanelView()
+    {
+        InitializeComponent();
+    }
+}
